f1908b3653a18957689fcbfe20adb9035024f693
root <root@institutoweb.virtuaserver.com.br>
2025-02-26 21:50:47 -0300

    Preparando para criar modulo de checkin

--- s7dadmin/error.php
+++ s7dadmin/error.php
@@ -7,3 +7,13 @@
 2025-01-20T14:14:00+00:00	INFO 189.6.6.244	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
 2025-01-20T18:30:27+00:00	INFO 200.130.2.5	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
 2025-01-20T23:54:33+00:00	INFO 189.61.91.98	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2025-01-22T18:02:20+00:00	INFO 168.228.200.126	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2025-01-26T01:36:27+00:00	INFO 191.217.170.102	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2025-01-31T12:23:51+00:00	INFO 177.134.132.192	joomlafailure	Não é permitido senha vazia.
+2025-01-31T12:24:15+00:00	INFO 177.134.132.192	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2025-01-31T12:24:29+00:00	INFO 177.134.132.192	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2025-01-31T12:24:53+00:00	INFO 177.134.132.192	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2025-01-31T12:25:19+00:00	INFO 177.134.132.192	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2025-01-31T12:25:22+00:00	INFO 177.134.132.192	joomlafailure	Não é permitido senha vazia.
+2025-01-31T12:25:53+00:00	INFO 177.134.132.192	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2025-01-31T12:26:01+00:00	INFO 177.134.132.192	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
